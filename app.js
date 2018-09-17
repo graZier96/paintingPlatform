74d5c0489e7fa9ccc8d416f19f4a7e2f6cc2559f
graZier96 <darkvicer0633@yahoo.com>
2018-09-17 15:34:24 +0800

    2018-9-17

--- app.js
+++ app.js
@@ -12,24 +12,6 @@ function loadModular(app,path,arr){   //app为主文件，path为app文件的路
 var arr = ['user','upload','paint','follow','commet','admin'];
 loadModular(app,'routes',arr);
 
-// var user = require("./routes/user");    //登录注册模块
-// app.use('/user',user);
-
-// var upload = require("./routes/upload");   //上传模块
-// app.use('/upload',upload);
-
-// var paint = require("./routes/paint");    //渲染图片模块
-// app.use('/paint',paint);
-
-// var follow = require("./routes/follow");    //关注模块
-// app.use('/follow',follow);
-
-// var commet = require("./routes/commet");     //评论模块
-// app.use('/commet',commet);
-
-// var admin = require("./routes/admin");   //管理员模块
-// app.use('/admin',admin);
-
 app.listen('8081',function(){
 	console.log("监听成功，地址为http://127.0.0.1:8081");
 });
